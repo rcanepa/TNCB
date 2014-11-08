234e0642767ab9177b7cf09ad52a1c8aa9a2e235
rcanepa <rcanepag@gmail.com>
2014-11-08 13:14:52 -0300

    Testing using the event module with jasmine

---
+++ src/filesizeWatcher.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var fs = require('fs');
+
+var FilesizeWatcher = function(path){
+	var self = this;
+
+	self.callbacks = {};
+
+	if( /^\//.test(path) === false ){
+		process.nextTick(function(){
+			self.callbacks['error']('Path does not start with a slash.');
+		});
+		return;
+	}
+
+	fs.stat(path, function(err, stats){
+		self.lastfilesize = stats.size;
+	});
+
+	self.interval = setInterval(
+		function(){
+			fs.stat(path, function(err, stats){
+				if(stats.size > self.lastfilesize){
+					self.callbacks['grew'](stats.size - self.lastfilesize);
+					self.lastfilesize = stats.size;
+				}
+				if(stats.size < self.lastfilesize){
+					self.callbacks['shrink'](self.lastfilesize - stats.size);
+					self.lastfilesize = stats.size;
+				}
+			
+		});
+			}, 1000);
+};
+
+FilesizeWatcher.prototype.on = function(eventType, callback){
+	this.callbacks[eventType] = callback;
+};
+
+FilesizeWatcher.prototype.stop = function(){
+	clearInterval(this.interval);
+};
+
+module.exports = FilesizeWatcher;
